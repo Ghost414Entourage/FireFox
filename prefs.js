fa3ce443d9e6307036522a2304f3d7fce428036b
Ghost <166275548+Ghost414Entourage@users.noreply.github.com>
2026-08-20 00:38:49 -0500

    Update prefs.js

--- prefs.js
+++ prefs.js
@@ -584,8 +584,6 @@ user_pref("security.ssl3.rsa_des_ede3_sha", false);
 // --- Group 38 ---
 user_pref("browser.cache.memory.enable", false);
 user_pref("browser.cache.memory.capacity", 0);
-user_pref("
-user_pref("
-user_pref("
-user_pref("
-user_pref("
+user_pref("network.dnsCacheEntries", 100);
+user_pref("network.dnsCacheExpiration, 60);
+user_pref("browser.cache.offline.enable, false);
