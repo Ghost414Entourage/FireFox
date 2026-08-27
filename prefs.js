5b22df08a406c644a6790949a70a145857ed1ffe
Ghost <166275548+Ghost414Entourage@users.noreply.github.com>
2026-08-27 01:25:04 -0500

    Add cookie preferences to prefs.js

--- prefs.js
+++ prefs.js
@@ -642,6 +642,9 @@ user_pref("dom.serviceWorkers.privateBrowsing.enabled", false);
 
 
 // --- Group 44 ---
+user_pref("network.cookie.maxageCap", 120);
+user_pref("network.cookie.maxPerHost", 0);
+user_pref("network.cookie.maxNumber", 0);
 user_pref("browser.cache.disk.max_entry_size", 0);
 user_pref("browser.cache.memory.max_entry_size", 0);
 user_pref("browser.history.maxStateObjectSize", 0);
