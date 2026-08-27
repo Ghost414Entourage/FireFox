59bc5844fd55e458630c4840d86c9ffd446cdc67
Ghost <166275548+Ghost414Entourage@users.noreply.github.com>
2026-08-26 22:13:29 -0500

    Add user preference for disk cache max entry size

--- prefs.js
+++ prefs.js
@@ -641,7 +641,7 @@ user_pref("dom.serviceWorkers.privateBrowsing.enabled", false);
 
 
 // --- Group 44 ---
-user_pref("
+user_pref("browser.cache.disk.max_entry_size", 0);
 user_pref("
 user_pref("
 user_pref("
