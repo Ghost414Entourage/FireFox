491c90cbd03a7aba160a295a65fcac18afaaad15
Ghost <166275548+Ghost414Entourage@users.noreply.github.com>
2026-08-30 13:05:15 -0500

    Update prefs.js

--- prefs.js
+++ prefs.js
@@ -468,7 +468,6 @@ user_pref("network.protocol-handler.expose.snews", false);
 user_pref("security.ssl.treat_unsafe_negotiation_as_broken", true);
 user_pref("security.ssl.require_safe_negotiation", true);
 user_pref("browser.xul.error_pages.expert_bad_cert", true);
-user_pref("security.tls.enable_0rtt_data", false);
 user_pref("security.tls.enable_kyber", true);
 user_pref("network.http.http3.enable_kyber", true);
 user_pref("privacy.resistFingerprinting.randomization.daily_reset.enabled", true);
